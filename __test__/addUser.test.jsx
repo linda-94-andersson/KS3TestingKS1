5ada1cb1c73903fab38176125e1c716ae31b3eb6
Linda Andersson <linda.29.andersson@gmail.com>
2022-12-13 21:49:22 +0100

    Small test of type of function

--- __test__/addUser.test.jsx
+++ __test__/addUser.test.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { describe, expect, it, vi } from "vitest";
-import { screen } from "@testing-library/react";
+import { screen, act, renderHook } from "@testing-library/react";
 import { render } from "./test-utils";
 import "@testing-library/jest-dom";
-import AddUser from "../src/components/User";
+import generated_id from "../src/components/User";
+import AddUser from "../src/components/AddUser";
 
 describe("AddUser component", () => {
-  it("Check uuid string", () => {
+  it("Check uuid function is running", () => {
     render(<AddUser />);
+    const GenId = generated_id;
+    expect(GenId).toBeTypeOf("function");
   });
 });
