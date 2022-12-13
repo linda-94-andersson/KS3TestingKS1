c35dd67a3f04b63913bbaa7936209e2e9069248f
Linda Andersson <linda.29.andersson@gmail.com>
2022-12-13 18:12:12 +0100

    Did first test with a function from User component

--- __test__/user.test.tsx
+++ __test__/user.test.tsx
@@ -1,12 +1,16 @@
 import React from "react";
-import { describe, expect, it } from "vitest";
+import { describe, expect, it, vi } from "vitest";
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import User from "../src/components/User";
+import handleDelete from "../src/components/User";
 
-describe("Sample App vitest", () => {
-  it("the title is visible", () => {
-    render(<User />);
-    expect(screen.getByText(/{u.name}/i)).toBeInTheDocument();
+//test för "handleDelete"
+describe("User handleDelete", () => {
+  it("delete is run and await next call", () => {
+    const handleDeleteSpy = vi.fn(handleDelete);
+    expect(handleDeleteSpy).toBeDefined();
   });
 });
+
+// test för första eller rendern
