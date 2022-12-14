4e0f8f7b73e5894929d91c3078e36132358e7ed9
Linda Andersson <linda.29.andersson@gmail.com>
2022-12-14 11:14:07 +0100

    New setup of testing components

---
+++ __test__/heading.test.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { describe, expect, it, vi } from "vitest";
+import { screen, fireEvent } from "@testing-library/react";
+import { render } from "./test-utils";
+import "@testing-library/jest-dom";
+import { Heading } from "@chakra-ui/react";
+
+describe("Test chakra heading", () => {
+  it("Heading render", () => {
+    const headingText = "test";
+    render(<Heading>{headingText}</Heading>);
+    const headingScreen = screen.getByText(headingText);
+    screen.debug();
+    expect(headingScreen).toBeVisible();
+  });
+});
