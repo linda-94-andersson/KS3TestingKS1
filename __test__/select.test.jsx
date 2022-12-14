e454f895b9f176f9369bed04ff83db666cca0156
Linda Andersson <linda.29.andersson@gmail.com>
2022-12-14 11:56:14 +0100

    Added test for chakra select

---
+++ __test__/select.test.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { describe, expect, it } from "vitest";
+import { screen } from "@testing-library/react";
+import { render } from "./test-utils";
+import "@testing-library/jest-dom";
+import { Select } from "@chakra-ui/react";
+
+describe("Test chakra select", () => {
+  it("should correctly set default option", () => {
+    render(
+      <Select>
+        <option>Select a ...</option>
+        <option>Test 1</option>
+        <option>Test 2</option>
+      </Select>
+    );
+    expect(screen.getByRole("option", { name: "Select a ..." }).selected).toBe(
+      true
+    );
+    screen.debug();
+  });
+  it("should display the correct number of options", () => {
+    render(
+      <Select>
+        <option>Select a ...</option>
+        <option>Test 1</option>
+        <option>Test 2</option>
+      </Select>
+    );
+    expect(screen.getAllByRole("option").length).toBe(3);
+  });
+});
