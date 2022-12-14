f7d6ad786e23e61117d08b4f208b56bc2528cb96
Linda Andersson <linda.29.andersson@gmail.com>
2022-12-14 12:57:05 +0100

    Setup for chakra text test

---
+++ __test__/text.test.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { describe, expect, it } from "vitest";
+import { screen } from "@testing-library/react";
+import { render } from "./test-utils";
+import "@testing-library/jest-dom";
+import { Text } from "@chakra-ui/react";
+
+describe("Test chakra text", () => {
+  it("Text render", () => {
+    const textText = "test";
+    render(<Text>{textText}</Text>);
+    const textScreen = screen.getByText(textText);
+    screen.debug();
+    expect(textScreen).toBeVisible();
+  });
+});
