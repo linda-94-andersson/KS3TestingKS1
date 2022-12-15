ffc503fb280a67c7c4b09fe9e82646583cb0b2b6
Linda Andersson <linda.29.andersson@gmail.com>
2022-12-15 15:33:34 +0100

    two new tests for projects and tasks

---
+++ __test__/integration_tests/tasks.test.jsx
@@ -0,0 +1,27 @@
+// Listan på Projects (1. går att se, 2. ta bort)
+// Listan på Tasks (1. går att se,  2. ta bort)
+// Listan på Users (1. går att se, 2. går att skapa)
+
+import React from "react";
+import { describe, expect, it, vi } from "vitest";
+import { screen, fireEvent } from "@testing-library/react";
+import { render } from "../test-utils";
+import "@testing-library/jest-dom";
+import Overview from "../../src/routes/Overview";
+
+describe("User intergration", () => {
+  beforeEach(() => {
+    vi.mock("../../__mock__/axios.js");
+  });
+
+  it("Tasks component render when Tasks tab is clicked", async () => {
+    render(<Overview />);
+    const userTabSelect = screen.getByRole("tab", { name: "Tasks" });
+    const clickTab = fireEvent.click(userTabSelect);
+    expect(clickTab).not.toBeFalsy();
+
+    screen.debug();
+  });
+
+  it("Tasks can be deleted", async () => {});
+});
