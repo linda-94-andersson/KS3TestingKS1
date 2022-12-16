abb23934ccdc651d303b130af8bf865a87fba711
Linda Andersson <linda.29.andersson@gmail.com>
2022-12-16 10:56:14 +0100

    Now able to see users

--- __test__/integration_tests/users.test.jsx
+++ __test__/integration_tests/users.test.jsx
@@ -3,26 +3,21 @@
 // Listan på Users (1. går att se, 2. går att skapa)
 
 import React from "react";
-import { describe, expect, it, vi } from "vitest";
-import { screen, fireEvent } from "@testing-library/react";
-import { render } from "../test-utils";
+import { describe, expect, it } from "vitest";
+import { screen, fireEvent, waitFor } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import Overview from "../../src/routes/Overview";
+import { customRender } from "../test-utils";
 
 describe("User intergration", () => {
-  beforeEach(() => {
-    vi.mock("../../__mock__/axios.js");
-  });
-
   it("Users component render when Users tab is clicked", async () => {
-    render();
-    render(<Overview />);
+    customRender(<Overview />);
     const userTabSelect = screen.getByRole("tab", { name: "Users" });
     const clickTab = fireEvent.click(userTabSelect);
     expect(clickTab).not.toBeFalsy();
-
+    await waitFor(() => screen.getByText("Linda"));
     screen.debug();
   });
 
-  it("User can be created", async () => {});
+  //it("User can be created", async () => {});
 });
