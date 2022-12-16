fb61fa68b16e6af82f672738406558b1324feb5a
Linda Andersson <linda.29.andersson@gmail.com>
2022-12-16 18:09:11 +0100

    Updated and added mock for all api endpoints

--- __mock__/handlers.js
+++ __mock__/handlers.js
@@ -1,3 +1,4 @@
+import { color } from "@chakra-ui/react";
 import { rest } from "msw";
 
 export const buildHandlers = (config) => {
@@ -16,7 +17,24 @@ export const buildHandlers = (config) => {
     startTime,
     endTime,
     taskId,
+    color,
   } = config;
+
+  let userList = [
+    {
+      id: idUser,
+      name: nameUser,
+    },
+  ];
+
+  let projectList = [
+    {
+      id: idProject,
+      name: nameProject,
+      color: color,
+      userId: userId,
+    },
+  ];
 
   let taskList = [
     {
@@ -27,57 +45,66 @@ export const buildHandlers = (config) => {
     },
   ];
 
+  let timelogsList = [
+    {
+      id: idTimelog,
+      startTime: startTime,
+      endTime: endTime,
+      taskId: taskId,
+    },
+  ];
+
   return [
+    //users
     rest.get(baseUrl + "/users", (req, res, ctx) => {
-      return res(
-        ctx.json([
-          {
-            id: idUser,
-            name: nameUser,
-          },
-        ])
-      );
+      return res(ctx.json(userList));
     }),
     rest.post(baseUrl + "/users", (req, res, ctx) => {
-      return res(
-        ctx.json([
-          {
-            id: idUser,
-            name: nameUser,
-          },
-        ])
-      );
+      return res(ctx.json(userList));
     }),
+    rest.delete(baseUrl + "/users/:id", (req, res, ctx) => {
+      userList = userList.filter((user) => user.id !== req.params.id);
+      return res(ctx.status(200));
+    }),
+
+    //projects
     rest.get(baseUrl + "/projects", (req, res, ctx) => {
-      return res(
-        ctx.json([
-          {
-            id: idProject,
-            name: nameProject,
-            userId: userId,
-          },
-        ])
-      );
+      return res(ctx.json(projectList));
     }),
+    rest.post(baseUrl + "/projects", (req, res, ctx) => {
+      return res(ctx.json(projectList));
+    }),
+    rest.delete(baseUrl + "/projects/:id", (req, res, ctx) => {
+      projectList = projectList.filter(
+        (project) => project.id !== req.params.id
+      );
+      return res(ctx.status(200));
+    }),
+
+    //tasks
     rest.get(baseUrl + "/tasks", (req, res, ctx) => {
+      return res(ctx.json(taskList));
+    }),
+    rest.post(baseUrl + "/tasks", (req, res, ctx) => {
       return res(ctx.json(taskList));
     }),
     rest.delete(baseUrl + "/tasks/:id", (req, res, ctx) => {
       taskList = taskList.filter((task) => task.id !== req.params.id);
-      console.log("DELETE", taskList);
       return res(ctx.status(200));
     }),
+
+    //timelogs
     rest.get(baseUrl + "/timelogs", (req, res, ctx) => {
-      return res(
-        ctx.json([
-          {
-            id: idTimelog,
-            startTime: startTime,
-            endTime: endTime,
-            taskId: taskId,
-          },
-        ])
+      return res(ctx.json(timelogsList));
+    }),
+    rest.post(baseUrl + "/timelogs", (req, res, ctx) => {
+      return res(ctx.json(timelogsList));
+    }),
+    rest.delete(baseUrl + "/timelogs/:id", (req, res, ctx) => {
+      timelogsList = timelogsList.filter(
+        (timelog) => timelog.id !== req.params.id
       );
+      return res(ctx.status(200));
     }),
   ];
 };
