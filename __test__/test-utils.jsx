d8f16b6e56c4bd47b702f69885322ddbbb15aabb
Linda Andersson <linda.29.andersson@gmail.com>
2022-12-16 10:54:36 +0100

    New render export

--- __test__/test-utils.jsx
+++ __test__/test-utils.jsx
@@ -20,11 +20,11 @@ const AllTheProviders = ({ children }) => {
   );
 };
 
-const customRender = (ui, options) =>
+export const customRender = (ui, options) =>
   render(ui, { wrapper: AllTheProviders, ...options });
 
 // re-export everything
 export * from "@testing-library/react";
 
 // override render method
-export { customRender as render };
+// export { customRender as render };
