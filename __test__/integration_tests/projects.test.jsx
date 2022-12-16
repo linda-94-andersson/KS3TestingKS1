182fabe26f7ea67b9a11e90f31b84ed1850a21de
Linda Andersson <linda.29.andersson@gmail.com>
2022-12-16 11:02:17 +0100

    Now able to see projects and tasks

--- __test__/integration_tests/projects.test.jsx
+++ __test__/integration_tests/projects.test.jsx
@@ -3,25 +3,21 @@
 // Listan på Users (1. går att se, 2. går att skapa)
 
 import React from "react";
-import { describe, expect, it, vi } from "vitest";
-import { screen, fireEvent } from "@testing-library/react";
+import { describe, expect, it } from "vitest";
+import { screen, fireEvent, waitFor } from "@testing-library/react";
 import { customRender } from "../test-utils";
 import "@testing-library/jest-dom";
 import Overview from "../../src/routes/Overview";
 
 describe("User intergration", () => {
-  beforeEach(() => {
-    vi.mock("../../__mock__/axios.js");
-  });
-
   it("Projects component render when Projects tab is clicked", async () => {
     customRender(<Overview />);
     const userTabSelect = screen.getByRole("tab", { name: "Projects" });
     const clickTab = fireEvent.click(userTabSelect);
     expect(clickTab).not.toBeFalsy();
-
+    await waitFor(() => screen.getByText("Filtrera tasks per datum"));
     screen.debug();
   });
 
-  it("Project can be deleted", async () => {});
+  // it("Project can be deleted", async () => {});
 });
--- __test__/integration_tests/tasks.test.jsx
+++ __test__/integration_tests/tasks.test.jsx
@@ -3,25 +3,21 @@
 // Listan på Users (1. går att se, 2. går att skapa)
 
 import React from "react";
-import { describe, expect, it, vi } from "vitest";
-import { screen, fireEvent } from "@testing-library/react";
+import { describe, expect, it } from "vitest";
+import { screen, fireEvent, waitFor } from "@testing-library/react";
 import { customRender } from "../test-utils";
 import "@testing-library/jest-dom";
 import Overview from "../../src/routes/Overview";
 
 describe("User intergration", () => {
-  beforeEach(() => {
-    vi.mock("../../__mock__/axios.js");
-  });
-
   it("Tasks component render when Tasks tab is clicked", async () => {
     customRender(<Overview />);
     const userTabSelect = screen.getByRole("tab", { name: "Tasks" });
     const clickTab = fireEvent.click(userTabSelect);
     expect(clickTab).not.toBeFalsy();
-
+    await waitFor(() => screen.getByText("Jag syns den 22 okt"));
     screen.debug();
   });
 
-  it("Tasks can be deleted", async () => {});
+  // it("Tasks can be deleted", async () => {});
 });
